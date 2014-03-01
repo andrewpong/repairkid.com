a6f9dcb106ccea8eb78c27cbdb7b3a79222efa68
AndrewPong <apong@dstcontrols.com>
2014-03-01 15:40:56 -0800

    HTML5 template for VS2013

---
+++ Repairkid/scripts/app.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    var serviceId = 'app';
+
+    // TODO: replace app with your module name
+    angular.module('app').factory(serviceId, ['$http', app]);
+
+    function app($http) {
+        // Define the functions and properties to reveal.
+        var service = {
+            getData: getData
+        };
+
+        return service;
+
+        function getData() {
+
+        }
+
+        //#region Internal Methods        
+
+        //#endregion
+    }
+})();
