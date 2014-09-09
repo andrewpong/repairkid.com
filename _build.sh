a7ce740879fab08a04a278d3245b09c29e48c25e
Andrew Pong <andrew@awp.io>
2014-09-09 16:01:21 -0700

    Auto-deploy Tue Sep  9 16:01:21 PDT 2014

--- _build.sh
+++ _build.sh
@@ -5,7 +5,7 @@ echo "Compiling complete! Committing to GitHub..."
 git add -A :/ && git commit -m "Auto-deploy `date`" && git push
 echo "Committed! Pushing to Divshot...";
 cd /Users/Hak/Repositories/repairkid.com/
-divshot deploy
+divshot push
 echo "Divshotted! Pushing to Firebase...";
 firebase deploy
 echo -e "Your base is fire!! Project deployed without a hitch.";
