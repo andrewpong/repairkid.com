cd6f200d5cd96091f03e4a42e50bb7703b0f82ec
Andrew Pong <andrew@awp.io>
2014-09-09 15:23:08 -0700

    deploy

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Compiling with harp to www"
+echo "Compiling with harp to /Users/Hak/Repositories/repairkid.com/www/..."
 harp compile /Users/Hak/Repositories/repairkid.com/_harp -o /Users/Hak/Repositories/repairkid.com/www
 echo "Compiling complete! Committing to GitHub..."
 git add -A && git commit -m 'deploy' && git push
