e760e4bfd68c6093553b53837231034ca595084f
Andrew Pong <andrew@awp.io>
2014-09-09 15:19:33 -0700

    deploy

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Compiling with harp to www"
+harp compile /Users/Hak/Repositories/repairkid.com/_harp -o /Users/Hak/Repositories/repairkid.com/www
+echo "Compiling complete! Committing to GitHub..."
+git add . && git commit -m 'deploy' && git push
+echo "Committed! Pushing to Firebase..."
+cd /Users/Hak/Repositories/repairkid.com/
+firebase deploy
+echo "Success! Your project was deployed without a hitch.";
