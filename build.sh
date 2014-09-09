ac66f6d07a658c8129d0322faba9789b9413fb36
Andrew Pong <andrew@awp.io>
2014-09-09 15:36:28 -0700

    Auto-deploy Tue Sep  9 15:36:28 PDT 2014

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 echo "Compiling with harp to /Users/Hak/Repositories/repairkid.com/www/..."
 harp compile /Users/Hak/Repositories/repairkid.com/_harp -o /Users/Hak/Repositories/repairkid.com/www
 echo "Compiling complete! Committing to GitHub..."
-git add -A && git commit -m "Auto-deploy `date`" && git push
+git add -A :/ && git commit -m "Auto-deploy `date`" && git push
 echo "Committed! Pushing to Firebase...";
 cd /Users/Hak/Repositories/repairkid.com/
 firebase deploy
