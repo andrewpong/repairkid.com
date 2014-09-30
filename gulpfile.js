10c732d4151ea52d5205d810b09937ea1f2a7f12
Andrew Pong <andrew@awp.io>
2014-09-30 15:40:29 -0700

    Auto-deploy Tue Sep 30 15:40:29 PDT 2014

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,41 @@
 var gulp = require('gulp');
-
-// Removes unused CSS
 var uncss = require('gulp-uncss');
+var minifyCSS = require('gulp-minify-css');
+var minifyHTML = require('gulp-minify-html');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
-gulp.task('uncss', function() {
-    return gulp.src('www/styles/theme.css')
-      .pipe(uncss({
+// Concatenate and minify all JS to all.js
+gulp.task('minify-js', function() {
+  gulp.src([
+    'www/scripts/jquery-1.11.1.js', 
+    'www/scripts/jquery.plugin.js', 
+    'www/scripts/bootstrap.js', 
+    'www/scripts/jquery.flexslider.js', 
+    'www/scripts/smoothscroll.js', 
+    'www/scripts/skrollr.js', 
+    'www/scripts/scrollreveal.js', 
+    'www/scripts/isotope.js', 
+    'www/scripts/scripts.js', 
+    ])
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'))
+});
+
+// Concatenate and minify all CSS to style.css
+gulp.task('minify-css', function() {
+  return gulp.src([
+    'www/styles/bootstrap.css', 
+    'www/styles/flexlider.css', 
+    'www/styles/lineicons.css', 
+    'www/styles/eleganticons.css', 
+    'www/styles/fontawesome.css', 
+    'www/styles/theme.css'
+    ])
+    .pipe(concat('all.css'))
+    // .pipe(gulp.dest('dist/scripts'))
+    .pipe(uncss({
           html: [
           'www/index.html',
           'www/sitemap.html',
@@ -16,26 +46,16 @@ gulp.task('uncss', function() {
           "www/services/index.html",
           "www/terms/index.html"
           ]
-      }))
-      .pipe(gulp.dest('dist/styles'));
+        }))
+    .pipe(minifyCSS({keepBreaks:false}))
+    .pipe(gulp.dest('dist/styles'));
 });
 
-// Compresses Javascript
-var uglify = require('gulp-uglify');
+// Minify all html files
+gulp.task('minify-html', function() {
+    var opts = {comments:true,spare:true};
 
-gulp.task('compress', function() {
-  gulp.src('www/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/scripts'))
-});
-
-// Concatenates files
-var concat = require('gulp-concat');
-
-gulp.task('concat', function() {
-  gulp.src([
-    'www/scripts/', 
-    './lib/file2.js'])
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./dist/'))
+  gulp.src('www/**/*.html')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('dist'))
 });
