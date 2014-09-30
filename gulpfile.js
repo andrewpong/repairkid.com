b6c88805d7367b17976f6fe5d9dedb97d3c85a6f
Andrew Pong <andrew@awp.io>
2014-09-30 14:09:32 -0700

    Auto-deploy Tue Sep 30 14:09:32 PDT 2014

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,18 @@ var uglify = require('gulp-uglify');
 gulp.task('uncss', function() {
     return gulp.src('www/styles/theme.css')
       .pipe(uncss({
-          html: ['www/index.html']
+          html: [
+          'www/index.html',
+          'www/sitemap.html',
+          'www/404.html',
+          "www/about/index.html",
+          "www/contact/index.html",
+          "www/privacy/index.html",
+          "www/services/index.html",
+          "www/terms/index.html"
+          ]
       }))
-      .pipe(gulp.dest('dist'));
+      .pipe(gulp.dest('dist/styles'));
 });
 
 gulp.task('compress', function() {
