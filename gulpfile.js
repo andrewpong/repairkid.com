8d85ecc9292deb29b29d53ae6e67e205a6ef81f3
Andrew Pong <andrew@awp.io>
2014-10-01 00:51:20 -0700

    Auto-deploy Wed Oct  1 00:51:20 PDT 2014

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var minifyCSS = require('gulp-minify-css');
 var minifyHTML = require('gulp-minify-html');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var pngcrush = require('imagemin-pngcrush');
 
 gulp.task('default', function() {
   gulp.start('minify-js', 'minify-css', 'minify-html');
@@ -62,4 +64,14 @@ gulp.task('minify-html', function() {
   gulp.src('www/**/*.html')
     .pipe(minifyHTML(opts))
     .pipe(gulp.dest('dist'))
+});
+
+gulp.task('minify-img', function () {
+    return gulp.src('www/images/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest('dist'));
 });
